01bfa3742a3e6ff13ecc057dc04a5afbda13defa
Çağrı Çatık <mccatik@gmail.com>
2024-04-07 04:20:16 +0200

    progress

---
+++ 0x_functions/functions.c
@@ -0,0 +1,9 @@
+#include "functions.h"
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int product(int a, int b) {
+    return a * b;
+}
---
+++ 0x_functions/functions.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int sum(int a, int b);
+int product(int a, int b);
+
+#endif
